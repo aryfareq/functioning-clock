e77f0beac16f41fe48e1d6afb59a6d31100ffd6b
ari <aryfareq03@gmail.com>
2023-01-18 23:50:52 +0300

    js debugged
    js fuctions have been defunctions debugged/no errors

--- clock.js
+++ clock.js
@@ -1,15 +1,17 @@
 function time(){
-    const now = Date();
+    const now = new Date();
     const second = now.getSeconds();
     const hand1 = ((second/60)*360) + 90;
-    document.getElementById("hand1").style.transform=`rotate (${hand1}deg)`;
+    document.getElementById("hand1").style.transform = `rotate(${hand1}deg)`;
+    console.log(hand1)
     
-    const minuts = now.getMinutes();
+    const minutes = now.getMinutes();
     const hand2= ((minutes/60)*360)+90;
-    document.getElementById("hand2").style.transform=`rotate (${hand2}deg)`;
+    document.getElementById("hand2").style.transform=`rotate(${hand2}deg)`;
     
     
-    const hour = now.getHour();
+    const hour = now.getHours();
     const hand3= ((hour/12)*360)+90;
-    document.getElementById.apply("hand3").style.transform=`rotate (${hand3}deg)`;
+    document.getElementById("hand3").style.transform=`rotate(${hand3}deg)`;
 }
+setInterval(time,1000);
